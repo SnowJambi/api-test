c8f9a341260392e36459c06b6570482bfca1cd89
Lachlan Jadezak <Lachlan@Jadezak.com>
2020-02-20 16:48:47 +1100

    add basic card styling to elements

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ async function getJson() {
 
 getJson().then(data => {
     for (const film of data) {
-        let div = document.createElement("div");
+            let div = document.createElement("div");
+        div.className = "card";
         let heading = document.createElement("h2");
         let description = document.createElement("p");
-        let root = document.getElementById("root");
+        let container = document.getElementById("container");
+        container.className = "container";
 
         heading.innerHTML = film.title;
         description.innerHTML = film.description;
 
+        
         div.appendChild(heading);
         div.appendChild(description);
-        root.appendChild(div);
+        container.appendChild(div);
     }
 });
